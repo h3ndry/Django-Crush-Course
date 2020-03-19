f6d0e688703e970badd1bea82a86148199c4423c
Henkho Dryza <simangahk@gmail.com>
2020-03-19 04:09:59 -0700

    the end it have a little problem, I will fix it later_

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -6,6 +6,23 @@ from .models import Question, Choice
 
 
 def index(request):
-    return render(request, 'polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
-# Create your views here.
+# Show specific question and Choice
+
+
+def detail(request, quedtion_id):
+    try:
+        question = Question.objects.get(pk=quedtion_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/results.html', {'question': question})
+
+
+# Get question and display results
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    # Create your views here.
